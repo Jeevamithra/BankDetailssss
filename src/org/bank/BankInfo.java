dfbd16ab9daca0c5b5cb0487ac318bc22983cabe
Jeevamithra <bhuvanamalar.36@gmail.com>
2021-12-01 13:52:45 +0530

    Bank details codes

---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,19 @@
+package org.bank;
+
+public abstract class BankInfo {
+	
+	public void savings() {
+		System.out.println("SAVING 3%");
+	}
+	
+	public void fixed() {
+		System.out.println("FIXED 5%");
+		
+
+	}
+	
+	//public void abstract deposti() 
+		
+	
+
+}
